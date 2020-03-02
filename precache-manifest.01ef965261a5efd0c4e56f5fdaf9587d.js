6f5c4c08a90696f724bed83aae65cad6e4b1e7f0
Dmytro Patalakha <dm.patalakha@gmail.com>
2020-03-02 23:14:54 +0100

    Updates

---
+++ precache-manifest.01ef965261a5efd0c4e56f5fdaf9587d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d0e6863dd8eed22068548733f705e6",
+    "url": "/BarberShop/index.html"
+  },
+  {
+    "revision": "fd580f71009bbb27d66c",
+    "url": "/BarberShop/static/css/main.310824fa.chunk.css"
+  },
+  {
+    "revision": "d1c96047e9d42128cc90",
+    "url": "/BarberShop/static/js/2.557af0ea.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/BarberShop/static/js/2.557af0ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd580f71009bbb27d66c",
+    "url": "/BarberShop/static/js/main.eaf8c5be.chunk.js"
+  },
+  {
+    "revision": "95c38d97daa95fa519ce",
+    "url": "/BarberShop/static/js/runtime-main.ae5a4343.js"
+  },
+  {
+    "revision": "26b8df0ddecd2770d691f36d16f5c63d",
+    "url": "/BarberShop/static/media/barber.26b8df0d.png"
+  },
+  {
+    "revision": "ad256565629f5efe45d66ec959a49dfa",
+    "url": "/BarberShop/static/media/bgBarbers.ad256565.jpg"
+  },
+  {
+    "revision": "7584a807d29dfe1f38ae6814adb36452",
+    "url": "/BarberShop/static/media/bgServices-desktop.7584a807.jpg"
+  },
+  {
+    "revision": "da8e7fe509571bdd351deb0f08b8b451",
+    "url": "/BarberShop/static/media/bg_general.da8e7fe5.jpg"
+  },
+  {
+    "revision": "033f901b2f971997d3ee87b290b675e1",
+    "url": "/BarberShop/static/media/bg_side_drawer.033f901b.jpg"
+  },
+  {
+    "revision": "c6b4ebcf786b4f71e96c21f7c5f082e8",
+    "url": "/BarberShop/static/media/empire.c6b4ebcf.svg"
+  },
+  {
+    "revision": "8ac1d3f173b9efeb8ca28fa0fe116790",
+    "url": "/BarberShop/static/media/logo.8ac1d3f1.png"
+  }
+]);
