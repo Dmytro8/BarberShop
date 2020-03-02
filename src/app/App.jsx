9cdbc0c489adee6e914361ddee2af20de89981fa
Dmytro Patalakha <dm.patalakha@gmail.com>
2020-03-02 20:02:53 +0100

    was initialized the app

---
+++ src/app/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { GeneralSection } from "../sections/GeneralSection";
+import "./App.scss";
+
+import bgBorderLeft from "../static/images/border_left.png";
+import { ServicesSectionContainer } from "../containers/ServicesSectionContainer";
+import { BarbersSectionContainer } from "../containers/BarbersSectionContainer";
+import { GallerySection } from "../sections/GallerySection";
+
+export const App = () => {
+  let bgBorderLeftStyle = {
+    backgroundImage: `url(${bgBorderLeft})`,
+    backgroundRepeat: "repeat-y",
+    left: "0px",
+    bottom: "0px",
+    top: "0px",
+    width: "4px"
+  };
+
+  return (
+    <div className="App">
+      <div className="borderRight" style={bgBorderLeftStyle}></div>
+      <GeneralSection />
+      <ServicesSectionContainer />
+      <BarbersSectionContainer />
+      <GallerySection />
+    </div>
+  );
+};
