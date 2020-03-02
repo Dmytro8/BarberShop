a51290435a3fc4397a7297cf4ddedbf533eb3293
Dmytro Patalakha <dm.patalakha@gmail.com>
2020-03-02 23:00:24 +0100

    the Contact section was created

---
+++ src/sections/ContactSection/ContactSection.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import classes from "./ContactSection.module.scss";
+
+export const ContactSection = () => {
+  return <section id="contact" className={classes.wrapper}></section>;
+};
