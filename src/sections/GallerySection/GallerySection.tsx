f4620e6aa16c05ab6d2ce44ce43471f398b13c2e
Dmytro Patalakha <dm.patalakha@gmail.com>
2020-03-02 23:00:00 +0100

    the Gallery section was created

---
+++ src/sections/GallerySection/GallerySection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import classes from "./GallerySection.module.scss";
+
+export const GallerySection = () => {
+  return (
+    <div id="gallery" style={{ height: "100vh", backgroundColor: "#dbbd9c" }}>
+      <div></div>
+    </div>
+  );
+};
